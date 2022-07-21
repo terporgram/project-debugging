9268ec9a5090058f64633da87c14d9e0436539ba
terporgram <193200076@almaata.ac.id>
2022-07-21 10:47:48 +0700

    debuging

---
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello, debugging!"
+        setContentView(R.layout.activity_main)
+        //logging()
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        val denominator = 4
+        repeat(5){
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+//    fun logging() {
+//        Log.e(TAG, "Error: a serious error like an app crash")
+//        Log.w(TAG, "WARN: warns about the potentioal for serious errors")
+//        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+//        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+//        Log.v(TAG, "BERBOSE: more verbose than DEBUG logs")
+//    }
+}
